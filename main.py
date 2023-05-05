111b994dbfba5e2e9970360afa7243303e712041
unZL <111940848+unZL@users.noreply.github.com>
2023-05-05 10:32:03 +0800

    支持gpt-3.5-turbo

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 import os
 import logging
+import utils.openai_utils as openai_utils
+from functools import lru_cache
 # import sys
 # 已经使用logging配置，这里弃用
 # # 设置日志级别为 DEBUG（调试模式）
@@ -49,101 +51,40 @@ async def startup_event():
     await before_server_start()
 
 # 读取配置文件中的 api key 并注入依赖
+# 为函数提供一个缓存机制，避免重复计算，提高函数性能。
+# @lru_cache(maxsize=None)
 def get_config(config: ai_config.Config = Depends(lambda:ai_config.Config(config_file_path))):
     return config
 
 # 配置读取
+# 为函数提供一个缓存机制，避免重复计算，提高函数性能。
+# @lru_cache(maxsize=None)
 def get_config_dict(all_config):
     # all_config = ai_config.Config(config_file_path)
 
     # 获取各个部分的配置
 
     # socks5_config = all_config.get_config("SOCKS5")
-
-    openai_completion_config = all_config.get_config("openai_Completion_config")
+    openai_api = all_config.get_config("openai_api")
+    openai_completion_davinci_config = all_config.get_config("openai_Completion_davinci_config")
+    openai_completion_turbo_config = all_config.get_config("openai_Completion_turbo_config")
     require_config = all_config.get_config("require_name")
-    return openai_completion_config,require_config
+    return openai_api,openai_completion_davinci_config,openai_completion_turbo_config,require_config
 
-
-
-# 创建 openai 请求
-def use_openai(openai_completion_config:dict=None,
-               prompts:str = None):
-    
-    if openai_completion_config == None:
-        return -1
-    if prompts == None:
-        return -1
-    # 获取 key
-    openai_key = openai_completion_config["api_key"]
-    
-    initial_prompt = openai_completion_config['initial_prompt']
-    temperature=float(openai_completion_config['temperature'])
-    max_tokens=int(openai_completion_config['max_tokens'])
-    top_p=float(openai_completion_config['top_p'])
-    frequency_penalty=float(openai_completion_config['frequency_penalty'])
-    presence_penalty=float(openai_completion_config['presence_penalty'])
-
-    openai.api_key = openai_key
-    # 借用国内代理进行跳转
-    openai.api_base = os.environ.get("OPENAI_API_BASE", "https://api.openai-proxy.com/v1")
-    response = openai.Completion.create(
-        model=openai_completion_config['model'],
-        prompt=initial_prompt+prompts,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        top_p=top_p,
-        frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty
-    )
-    # response = openai_key
-    return response
-
-# 调用 api
-def processing_demands(
-        openai_completion_config,
-        require_config,
-        require_name,
-        law_text):
-    # 获取任务名
-    require = require_config[f"require_{require_name}"]
-    # require = openai_config.get("require_name","require_"+require_name)
-    
-    if require == None:
-        return "require == None"
-    
-    # 输入参数，获取回答 response
-    prompts = require + "\n" + law_text
-    response = use_openai(openai_completion_config,prompts)
-
-    if response == -1:
-        return "openai_config == None | require_name == None | law_text == None !"
-    
-    if type(response) != type(""):
-        response_text = str(response.choices[0].text.strip())
-        response = response_text
-
-    # 获取任务类型
-    require_type = require_config[f"require_type_{require_name}"]
-    # require_type = openai_config.get("require_name","require_type_"+require_name)
-    res = {"材料文本":law_text,"任务类型":require_type,"回答":response}
-    # print(res)
-    return res
-
-
-# 使用 get 请求
-# 
-@app.get('/get/r/{require_name}/{law_text}')
-def text_extract(
-    config:ai_config.Config = Depends(get_config),
-    law_text: str = None,
-    require_name: str = None
-    ):
-    openai_completion_config,require_config = get_config_dict(config)    
-    # 启动 openai
-    res = processing_demands(openai_completion_config,require_config,require_name,law_text)
-    return res
-
+{
+  # 使用 get 请求
+    # 
+    # @app.get('/get/r/{require_name}/{law_text}')
+    # def text_extract(
+    #     config:ai_config.Config = Depends(get_config),
+    #     law_text: str = None,
+    #     require_name: str = None
+    #     ):
+    #     openai_completion_config,require_config = get_config_dict(config)    
+    #     # 启动 openai
+    #     res = openai_utils.processing_demands(openai_completion_config,require_config,require_name,law_text)
+    #     return res  
+}
 
 # 使用 post 请求
 
@@ -152,22 +93,56 @@ class Law_item_require(BaseModel):
     require_name: str
     law_text: str
 
-
-# 
+# 使用模型： text-davinci-003
 @app.post('/post/r/')
-def text_extract(
+def text_extract_davinci(
     config:ai_config.Config = Depends(get_config),
     law_item_require: Law_item_require = None
     ):
     if law_item_require == None:
         return "post == None !"
+ 
+    openai_api,openai_completion_davinci_config,openai_completion_turbo_config,require_config = get_config_dict(config)
+    # 初始化  
+    davinci = openai_utils.gpt_requst_davinci(
+        openai_api,
+        openai_completion_davinci_config,
+        law_item_require
+        )
+    # 通过 law_item 获取参数
+    require,require_type,prompts = davinci.get_require_by_law_item(require_config)
 
-    require_name = law_item_require.require_name
-    law_text = law_item_require.law_text
 
-    openai_completion_config,require_config = get_config_dict(config)    
     # 启动 openai
-    res = processing_demands(openai_completion_config,require_config,require_name,law_text)
+    res = davinci.processing_demands(require,require_type,prompts)
+
+    return res
+
+
+# 使用模型： gpt-3.5-turbo
+@app.post('/post/r/turbo/')
+def text_extract_turbo(
+    config:ai_config.Config = Depends(get_config),
+    law_item_require: Law_item_require = None
+    ):
+    if law_item_require == None:
+        return "post == None !"
+ 
+    openai_api,openai_completion_davinci_config,openai_completion_turbo_config,require_config = get_config_dict(config)
+
+    # 初始化  
+    turbo = openai_utils.gpt_requst_turbo(
+        openai_api,
+        openai_completion_turbo_config,
+        law_item_require
+        )
+
+    # 通过 law_item 获取参数
+    require,require_type,prompts = turbo.get_require_by_law_item(require_config)
+
+
+    # 启动 openai
+    res = turbo.processing_demands(require,require_type,prompts)
     return res
 
 # 捕获参数校验异常
