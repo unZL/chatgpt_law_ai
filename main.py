63c827b216033130845dc0ca92c9acea0c57b6f4
unZL <111940848+unZL@users.noreply.github.com>
2023-05-12 15:44:22 +0800

    更新日志提示和保存

--- main.py
+++ main.py
@@ -20,31 +20,51 @@ from functools import lru_cache
 config_file_path = "config.ini"
 log_config_file_path = "logging.ini"
 log_name = "ai_app"
-ai_log = law_ai_logger.ai_log(log_config_file_path,log_name)
+log_filedir_path = './log/'
+log_file_path = "chatgpt_law_ai_log.log"
 
-openai_logger = logging.getLogger('openai')
-openai_logger.addHandler(logging.handlers.TimedRotatingFileHandler('./log/chatgpt_law_ai_log.log'))
+# 端口和地址
+api_host="127.0.0.1"
+api_port=8080
+
+
+def genarate_log(log_config_file_path,log_name,log_filedir_path,log_file_path):
+    
+    ai_log = law_ai_logger.ai_log(log_config_file_path,log_name)
+    openai_logger = logging.getLogger('openai')
+    openai_logger.addHandler(logging.handlers.TimedRotatingFileHandler(log_filedir_path + log_file_path))
+
+
 
 # 创建一个 FastAPI 对象
 app = FastAPI()
 
 # 如果出现连接失败，请使用代理
 # 不使用代理有可能被封禁
-# 下面的函数将开启 socks5 代理
+
 
 # 在服务器启动前执行此函数
 async def before_server_start():
+
     # 可以在此处执行一些初始化操作
-    print("服务器正在启动中……")
-    
-    # 下面的函数将开启 socks5 代理 如不需要请将其注释
-    # socks5_config = socks5_utils.Socks5_config(config_file_path)
-    # # 执行 Socks5 配置对象的初始化方法
-    # # socks5_config.init()
-    # # 打印代理信息
-    # socks5_config.print_info_proxy()
+    print("服务器正在启动中……\n\n")
+    # 检查日志文件夹
+    if os.path.exists(log_filedir_path):
+        # 创建日志
+        genarate_log(log_config_file_path,log_name,log_filedir_path,log_file_path)
+        log_path = os.path.abspath(log_filedir_path + log_file_path)
+        print(f"日志创建完成,日志目录位于：{log_path}")
+    else:
+        print("缺失日志文件夹!\n")
+        os.makedirs(log_filedir_path)
+        print("创建日志文件夹")
+        # 创建日志
+        genarate_log(log_config_file_path,log_name,log_filedir_path,log_file_path) 
+        log_path = os.path.abspath(log_filedir_path + log_file_path)
+        print(f"日志创建完成,日志目录位于：{log_path}")
+        
     print("服务器启动完成")
-    print("可以通过：localhost:端口//state 查看接口状态")
+    print(f"可以通过：{api_host}:{api_port}/state 查看接口状态")
 
 
 
@@ -53,14 +73,10 @@ async def startup_event():
     await before_server_start()
 
 # 读取配置文件中的 api key 并注入依赖
-# 为函数提供一个缓存机制，避免重复计算，提高函数性能。
-# @lru_cache(maxsize=None)
 def get_config(config: ai_config.Config = Depends(lambda:ai_config.Config(config_file_path))):
     return config
 
 # 配置读取
-# 为函数提供一个缓存机制，避免重复计算，提高函数性能。
-# @lru_cache(maxsize=None)
 def get_config_dict(all_config):
     # all_config = ai_config.Config(config_file_path)
 
@@ -73,20 +89,6 @@ def get_config_dict(all_config):
     require_config = all_config.get_config("require_name")
     return openai_api,openai_completion_davinci_config,openai_completion_turbo_config,require_config
 
-{
-  # 使用 get 请求
-    # 
-    # @app.get('/get/r/{require_name}/{law_text}')
-    # def text_extract(
-    #     config:ai_config.Config = Depends(get_config),
-    #     law_text: str = None,
-    #     require_name: str = None
-    #     ):
-    #     openai_completion_config,require_config = get_config_dict(config)    
-    #     # 启动 openai
-    #     res = openai_utils.processing_demands(openai_completion_config,require_config,require_name,law_text)
-    #     return res  
-}
 
 # 使用 post 请求
 
@@ -165,10 +167,11 @@ def app_start_state():
     return "服务已启动  当前时间：" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
  
 if __name__ == '__main__':
+
     # 配置 uvicorn 服务器
     uvicorn_config = Config(app=app,
-                host="127.0.0.1",
-                port=8080,
+                host=api_host,
+                port=api_port,
                 workers=1)
     # 创建服务器实例
     server = Server(uvicorn_config)
